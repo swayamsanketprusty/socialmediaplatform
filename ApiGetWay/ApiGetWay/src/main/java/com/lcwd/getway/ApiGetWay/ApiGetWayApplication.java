6f911d13b19351bc36f64dfa63b00fc008c9a6c9
swayamsanketprusty <127466676+swayamsanketprusty@users.noreply.github.com>
2023-09-28 18:49:06 +0530

    Add files via upload

---
+++ ApiGetWay/ApiGetWay/src/main/java/com/lcwd/getway/ApiGetWay/ApiGetWayApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.getway.ApiGetWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGetWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGetWayApplication.class, args);
+	}
+
+}
---
+++ ApiGetWay/ApiGetWay/src/test/java/com/lcwd/getway/ApiGetWay/ApiGetWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.getway.ApiGetWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
